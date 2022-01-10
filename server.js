f1a877b4a487c663783adcd0b95bb2bd5c70f09f
Owen Kelvin <otienoowenkelvin@gmail.com>
2022-01-10 10:03:34 +0300

    adds server.js config

---
+++ server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 5555;
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/apps/lpg-admin"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/apps/lpg-admin/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+console.log( `App is running on port ${ port }` )
+app.listen( port );
+
+
